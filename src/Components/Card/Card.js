c3d71ae90141edcfee72075cb8ca2a855a72aa55
anna-shr <annashevelevar@gmail.com>
2023-06-15 21:19:59 +0300

    add card selection with react useState

--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -1,13 +1,20 @@
+import React, { useState } from 'react';
 import './Card.css';
 
 function Card(props) {
+  const [selected, toggleSeletion] = useState(false);
+
+  const handleHover = () => {
+    toggleSeletion(!selected);
+  };
+
   return (
-    <div className={"card-wrapper " +(props.selected?"selected":"")}>
-      <div className={"card  top "+(props.color?props.color:"")}>
+    <div className={"card-wrapper " + (selected ? "selected" : "")} onMouseEnter={handleHover} onMouseLeave={handleHover}>
+      <div className={"card  top " + (props.color ? props.color : "")}>
         <p className='name'>{props.name}</p>
       </div>
-      <div className={"card "+(props.color?props.color:"")}>
-      <p className='small left'>руб</p>
+      <div className={"card " + (props.color ? props.color : "")}>
+        <p className='small left'>руб</p>
         <p className='price'>{props.price}</p>
         <p className='small right'>/мес</p>
       </div>
--- src/pricesList.js
+++ src/pricesList.js
@@ -1,8 +1,8 @@
 const data = [
-    { "name": "Безлимитный 300", "price": "300", "speed": "До 10 Мбит/сек", "selected": false, "color": "blue" },
-    { "name": "Безлимитный 450", "price": "450", "speed": "До 50 Мбит/сек", "selected": false, "color": "green" },
-    { "name": "Безлимитный 550", "price": "550", "speed": "До 100 Мбит/сек", "selected": true, "color": "red" },
-    { "name": "Безлимитный 1000", "price": "1000", "speed": "До 200 Мбит/сек", "selected": false, "color": "black" },
+    { "name": "Безлимитный 300", "price": "300", "speed": "До 10 Мбит/сек", "color": "blue" },
+    { "name": "Безлимитный 450", "price": "450", "speed": "До 50 Мбит/сек", "color": "green" },
+    { "name": "Безлимитный 550", "price": "550", "speed": "До 100 Мбит/сек", "color": "red" },
+    { "name": "Безлимитный 1000", "price": "1000", "speed": "До 200 Мбит/сек", "color": "black" },
 ]
 
 export default data;
